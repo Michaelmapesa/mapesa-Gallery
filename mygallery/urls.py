5d74eddefec170318f5542955b652dac43df8601
michael mapesa <michaelmapesa93@gmail.com>
2022-03-26 14:58:43 +0300

    adding functions

--- mygallery/urls.py
+++ mygallery/urls.py
@@ -4,7 +4,7 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns=[
-    path(' ',views.index,name = 'index'),
+    path('',views.index,name = 'index'),
     re_path(r'^search/', views.search_image, name='search_image'),
     re_path(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter'),
     re_path(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single,name = 'single')
