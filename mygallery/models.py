3e4113a70156a3ae76138ecce1b7e16472fc99e2
michael mapesa <michaelmapesa93@gmail.com>
2022-03-26 07:30:03 +0300

    add image class and its class methods

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,5 +1,6 @@
 from unicodedata import name
 from django.db import models
+from django.forms import ImageField
 
 
 class Location(models.Model):
@@ -49,5 +50,46 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    name=models.CharField(max_length=60)
+    pic = models.ImageField(upload_to='upolads/')
+    picture= ImageField(black=True, manual_crop='1920x1080')
+    description=models.TextField()
+    image_location=models.ForeignKey('Location')
+    image_category=models.ForeignKey('Category')
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_all_images(cls):
+        images=cls.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls,id):
+        image=cls.objects.filter(id=id).all()
+        return image
+
+    @classmethod
+    def search_by_category(cls,image_category):
+        images=Image.objects.filter(image_category__name__icontains=image_category)
+        return images
+
+    @classmethod
+    def filter_by_location(cls, image_location):
+        images_location = cls.objects.filter(image_location__id=image_location)
+        return images_location
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering=['name']
+
 
 # Create your models here.
