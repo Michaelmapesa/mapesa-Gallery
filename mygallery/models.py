98146525d581f22e1d7a62055aaf7a99a74b5805
michael mapesa <michaelmapesa93@gmail.com>
2022-03-26 06:46:42 +0300

    add category class and its class method

--- mygallery/models.py
+++ mygallery/models.py
@@ -1,3 +1,4 @@
+from unicodedata import name
 from django.db import models
 
 
@@ -23,6 +24,28 @@ class Location(models.Model):
     def get_location_id(cls, id):
         locate = Location.objects.get(pk=id)
         return locate
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name=models.CharField(max_length=50)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self, update):
+        self.name=update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls,id):
+        category=Category.objects.get(pk=id)
+        return category
 
     def __str__(self):
         return self.name
