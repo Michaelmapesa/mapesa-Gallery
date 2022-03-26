38e0f81cae35c17e1cdc97d0e7e81ae9ae47bc5b
michael mapesa <michaelmapesa93@gmail.com>
2022-03-26 09:22:29 +0300

    creating index view

--- mygallery/views.py
+++ mygallery/views.py
@@ -1,4 +1,15 @@
+from turtle import title
 from django.shortcuts import render,redirect 
 from django.http import HttpResponse, Http404
+from .models import Image, Location, Category
 
 # Create your views here.
+def index(request):
+    images=Image.get_all_images()
+    locations=Location.objects.all()
+    title= 'My-Gallery'
+
+    return render(request, 'index.html',{'title':title, 'images':images,'locations':locations})
+
+def single
+
